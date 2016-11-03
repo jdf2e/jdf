1d035cd1010e6b0dc76d87e39f0c51c8a6a0f70a
liujia <liujia30@jd.com>
2016-11-03 19:23:18 +0800

    多type选择的测试用例

---
+++ test/widget/widgetInHTML_multiple_types/widgetInHTML_multiple_types.js
@@ -0,0 +1,6 @@
+$(function () {
+    var tpl = 'This page has loaded widgetInHTML_multiple_types.js<br>'
+                + 'if this words are blue, <br>'
+                + 'means this page has loaded widgetInHTML_multiple_types.css';
+    $('.multitypes').html(tpl);
+});
