d5757aefbe76d2e4acd8605fab39cbed5f11b9a6
liujia <liujia30@jd.com>
2016-12-07 17:51:52 +0800

    将文件类型剥离出来；修复widget编译到html引用js,css错误

---
+++ test/jdf_vfs_virtualfilesystem.js
@@ -0,0 +1,12 @@
+'use strict';
+const expect = require('expect.js');
+const path = require('path');
+const os = require('os');
+const utils = require('jdf-utils');
+const f = utils.file;
+const VFile = require('../lib/VFS/VirtualFile.js');
+const VFS = require('../lib/VFS/VirtualFileSystem.js');
+
+describe('Virtual File System (VFS)', () => {
+
+});
