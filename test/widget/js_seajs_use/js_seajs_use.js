42524f1e5437815d12534e5a511674cc1d485170
chenxiaochun <sjzchenxiaochun@126.com>
2016-12-13 13:37:54 +0800

    在test中添加测试实例

--- test/widget/js_seajs_use/js_seajs_use.js
+++ test/widget/js_seajs_use/js_seajs_use.js
@@ -1,8 +1,10 @@
 
 seajs.use(["test1.js", "/test2.js", "../test3.js", "./test4.js"], function(a, b, c, d){
-	
+
 });
 
 seajs.use(["jdf/1.0.0/unit/globalInit/2.0.0/globalInit.js"], function(globalInit){
 
 });
+
+seajs.use('../common/common.js');
