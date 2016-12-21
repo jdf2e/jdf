a81c6cff029413f91d2855f570e5548d9caceb6f
liujia <liujia30@jd.com>
2016-12-21 21:17:16 +0800

    一组测试文件，未写完

---
+++ test/vfs/virtual-file.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const expect = require('expect.js');
+
+const VFile = require('../../lib/VFS/VirtualFile');
+
+describe('测试VFile', function () {
+    describe('path invalid', function () {
+
+    });
+});
