c3b6f8a49d0fa7797590dfd6d2b6ccb180de3ad8
chenxiaochun <sjzchenxiaochun@126.com>
2016-12-23 17:56:54 +0800

    add test unit js_cmd_define

--- test/urlReplace/index.js
+++ test/urlReplace/index.js
@@ -2,15 +2,38 @@
 
 const expect = require('expect.js');
 const urlReplace = require('../../lib/urlReplace');
+const jdf = require('../../lib/jdf.js');
 const f = require('jdf-utils').file;
 
 describe('replace url', function(){
-    describe('comboUrlPath', function(){
+    describe('comboUrlPath()', function(){
         it('comboUrlPath is ok', function(){
-            var origin = f.read('test/urlReplace/comboUrl_origin.html');
-            var result = f.read('test/urlReplace/comboUrl_result.html');
+            var case01 = f.read('test/urlReplace/comboUrlPath/case01.html');
+            var result01 = f.read('test/urlReplace/comboUrlPath/result01.html');
 
-            expect(urlReplace.comboUrlPath(origin)).to.equal(result);
+            expect(urlReplace.comboUrlPath(case01)).to.equal(result01);
         });
     });
+
+    describe('js_cmd_define', function(){
+        var source = '/Users/chenxiaochun/Documents/MyProject/jdf/build/jdf-test/widget/js_cmd_define/js_cmd_define.js';
+
+        it('case01 is ok', function(){
+            var case01 = f.read('test/urlReplace/js_cmd_define/case01.js');
+            var result01 = f.read('test/urlReplace/js_cmd_define/result01.js');
+
+            jdf.config.projectPath = 'jdf-test';
+
+            expect(urlReplace.addJsDepends(source, case01)).to.equal(result01);
+        });
+
+        it('case02 is ok', function(){
+            var case02 = f.read('test/urlReplace/js_cmd_define/case02.js');
+            var result02 = f.read('test/urlReplace/js_cmd_define/result02.js');
+
+            jdf.config.projectPath = 'jdf-test';
+
+            expect(urlReplace.addJsDepends(source, case02)).to.equal(result02);
+        });
+    })
 });
---
+++ test/urlReplace/js_cmd_define/result02.js
@@ -0,0 +1,3 @@
+define("//misc.360buyimg.com/jdf-test/widget/js_cmd_define/js_cmd_define.js",["//misc.360buyimg.com/jdf-test/k.js","//misc.360buyimg.com/jdf-test/widget/js_cmd_define/a.js","//misc.360buyimg.com/jdf-test/b.js"],function(require, exports, module){
+    var k = require("//misc.360buyimg.com/jdf-test/k.js");
+})
